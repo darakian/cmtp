41a06cbfdd2aa6fbf7a5e79ebf27db257d6200b4
Jon Moroney <jmoroney@hawaii.edu>
2016-02-09 10:42:41 -1000

    Added server key response to KEYREQUEST

--- source/server_functions.c
+++ source/server_functions.c
@@ -42,10 +42,11 @@ const char cmtp_command_NOOP[] = {"NOOP\n"};
 const char cmtp_command_OBAI[] = {"OBAI\n"};
 const char cmtp_command_KEYREQUEST[] = {"KEYREQUEST\n"};
 const char zero_byte = '\0';
+const char crypto_version[4] = {0};
 char home_domain[64] = {0};
 uint32_t MAX_CONNECTIONS = 10;
-static char server_public_key[64] = {0};
-static char server_private_key[64] = {0};
+static unsigned char server_public_key[64] = {0};
+static unsigned char server_private_key[64] = {0};
 
 /*
 Ensure that server has everything it needs to begin operation.
@@ -239,7 +240,15 @@ void * connection_manager(void * connection_manager_argument)
       } while((i<sizeof(user_keyrequest_buffer))&&(user_keyrequest_buffer[i-1]!='\0'));
       if (memcmp(&user_keyrequest_buffer, &zero_byte, 1)==0)
       {
+        unsigned char signature[crypto_sign_BYTES];
+        crypto_sign_detached(signature, NULL, server_public_key, sizeof(server_public_key), server_private_key);
         //Wants server key. Reply and return.
+        //Send crypto_type first
+        write(thread_connection, crypto_version, sizeof(crypto_version));
+        //Then the public key
+        write(thread_connection, server_public_key, sizeof(server_public_key));
+        //Then the signed public key
+        write(thread_connection, signature, sizeof(signature));
       }
 
       uint32_t base64_username_length = base64_encode((char *)user_keyrequest_buffer, sizeof(user_keyrequest_buffer), base64_username, sizeof(base64_username), (char *)filesystem_safe_base64_string, 64);
