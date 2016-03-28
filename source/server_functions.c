55a2cca54496aa912243f2fff70502ffb511b886
Jon Moroney <jmoroney@hawaii.edu>
2016-03-28 09:37:10 -1000

    Adding some debug logic to the server

--- source/server_functions.c
+++ source/server_functions.c
@@ -460,8 +460,6 @@ int32_t keyrequest_responder(uint32_t socket)
     i++;
   } while((i<sizeof(user_keyrequest_buffer))&&(user_keyrequest_buffer[i-1]!='\0'));
   if (memcmp(user_keyrequest_buffer, &termination_char, 1)==0)
-  //try this
-  //if(user_keyrequest_buffer[0]==0)
   {
     crypto_sign_detached(signature_of_public_key, NULL, server_public_key, sizeof(server_public_key), server_private_key);
     //Wants server key. Reply and return.
@@ -472,6 +470,9 @@ int32_t keyrequest_responder(uint32_t socket)
     return 0;
     //Need to end here. Might need to functionize this code.
   }
+  #ifdef DEBUG
+  printf("Requested user = %s\n", user_keyrequest_buffer);
+  #endif /*DEBUG*/
   else
   {
     uint32_t base64_username_length = base64_encode((char *)user_keyrequest_buffer, strlen(user_keyrequest_buffer), base64_username, strlen(base64_username), (char *)filesystem_safe_base64_string, 64);
