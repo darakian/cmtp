5bd9482c8f8bd779f3173616c4bafb2c64306ed4
Jon Moroney <jmoroney@hawaii.edu>
2016-03-28 09:43:28 -1000

    Adding MORE debug logic to the server

--- source/server_functions.c
+++ source/server_functions.c
@@ -472,10 +472,11 @@ int32_t keyrequest_responder(uint32_t socket)
   }
   else
   {
+    uint32_t base64_username_length = base64_encode((char *)user_keyrequest_buffer, strlen(user_keyrequest_buffer), base64_username, strlen(base64_username), (char *)filesystem_safe_base64_string, 64);
     #ifdef DEBUG
     printf("Requested user = %s\n", user_keyrequest_buffer);
+    printf("Requested user in base64 = %s\n", base64_username);
     #endif /*DEBUG*/
-    uint32_t base64_username_length = base64_encode((char *)user_keyrequest_buffer, strlen(user_keyrequest_buffer), base64_username, strlen(base64_username), (char *)filesystem_safe_base64_string, 64);
     do {
       read(socket, domain_keyrequest_buffer+i, 1);
       i++;
