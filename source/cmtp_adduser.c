57b5fa6c95635c559319a36fca19dcec562d8637
Jon Moroney <jmoroney@hawaii.edu>
2016-04-27 10:43:25 -1000

    More work on adduser

--- source/cmtp_adduser.c
+++ source/cmtp_adduser.c
@@ -8,6 +8,8 @@
 
 int main(int argc, char * argv[])
 {
+  char yes_no[3] = {0};
+  char user_path[256+15] = {0};
   if (argc<=1)
   {
     /*Interactive mode. To be done later.*/
@@ -18,11 +20,19 @@ int main(int argc, char * argv[])
   {
     printf("Usage: cmtp_adduser <username>\n");
   }
-  char yes_no[3] = {0};
+
   printf("Create cmtp user %s?\n", argv[1]);
   prompt_input_string("y/n: ", " ", yes_no, 1);
   #ifdef DEBUG
-  printf("Attempting to create user %s in /var/cmtp\n", argv[1]);
+  printf("Attempting to create user %s in /var/cmtp/mail/\n", argv[1]);
   #endif /*DEBUG*/
-  if (access())
+  if (snprintf(user_path, sizeof(user_path), "%s%s", "/var/cmtp/mail/", argv[1])<0)
+  {
+    perror("snprintf");
+    return -1;
+  }
+  if (access(user_path, R_OK)>=0)
+  {
+    printf("User %s directory already exists at %s\n", argv[1], user_path);
+  }
 }
