d5df354094aeb3b6ede7605e743e3f2e7cd0976c
Jon Moroney <jmoroney@hawaii.edu>
2016-04-27 10:35:07 -1000

    Adding first code path for user creation

--- source/cmtp_adduser.c
+++ source/cmtp_adduser.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include "cmtp_common.h"
 
 int main(int argc, char * argv[])
@@ -5,13 +11,18 @@ int main(int argc, char * argv[])
   if (argc<=1)
   {
     /*Interactive mode. To be done later.*/
+    printf("Interactive mode not yet working :(\n");
     return 0;
   }
   if (argc>=3)
   {
     printf("Usage: cmtp_adduser <username>\n");
   }
-
+  char yes_no[3] = {0};
   printf("Create cmtp user %s?\n", argv[1]);
-
+  prompt_input_string("y/n: ", " ", yes_no, 1);
+  #ifdef DEBUG
+  printf("Attempting to create user %s in /var/cmtp\n", argv[1]);
+  #endif /*DEBUG*/
+  if (access())
 }
