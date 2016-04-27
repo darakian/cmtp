4ee3ad41b802431264849c8f3125ad7b7714c124
Jon Moroney <jmoroney@hawaii.edu>
2016-04-27 09:57:03 -1000

    Initial work on adduser utility

--- source/cmtp_adduser.c
+++ source/cmtp_adduser.c
@@ -2,5 +2,14 @@
 
 int main(int argc, char * argv[])
 {
-  
+  if (argc<=1)
+  {
+    /*Interactive mode*/
+    return 0;
+  }
+  if (argc>=3)
+  {
+    //Too many arguments
+    printf("Usage: cmtp_adduser <username>\n");
+  }
 }
