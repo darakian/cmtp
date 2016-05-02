8bb152b05e8801f8ec10f77d85a5d2c27e1c5188
Jon Moroney <jmoroney@hawaii.edu>
2016-05-02 11:13:31 -1000

    work on xzibit building. Need an update on libsodium to test

--- source/cmtp_adduser.c
+++ source/cmtp_adduser.c
@@ -17,7 +17,7 @@ int main(int argc, char * argv[])
   char yes_no[3] = {0};
   char user_path[256+15] = {0};
   char user_publickey_path[256+15+10] = {0};
-  char user_privatekey_path[256+15+10] = {0};
+  char user_xzibit_path[256+15+10] = {0};
   char user_password[256] = {0};
   int temp_descriptor = -1;
   char user_password_hash[crypto_aead_aes256gcm_KEYBYTES] = {0}; //size = 32
@@ -100,9 +100,35 @@ int main(int argc, char * argv[])
 
   //Symetric cipher with hashed user_password
   unsigned char ciphertext[MESSAGE_LEN + crypto_aead_aes256gcm_ABYTES];
+  unsigned char xzibit[sizeof(ciphertext+4)];
   if (crypto_aead_aes256gcm_encrypt(ciphertext,sizeof(ciphertext), xzibit, sizeof(xzibit), NULL, 0, NULL, nonce, key)<0)
   {
     perror("crypto_aead_aes256gcm_encrypt");
+  }
+  //Need to append the version and other envelope information to xzibit here
+
+  if (snprintf(user_xzibit_path, sizeof(user_xzibit_path), "%s%s%s%s%s", "/var/cmtp/mail/", argv[1], "/", argv[1], ".xzibit")<0)
+  {
+    perror("snprintf");
+    return -1;
+  }
+  if ((temp_descriptor=open(user_xzibit_path, O_WRONLY|O_CREAT, S_IRUSR|S_IRGRP|S_IROTH))<0)
+  {
+    perror("open");
+    printf("Cannot write out user public key. Exiting.\n");
+    return -1;
+  }
+  if (write(temp_descriptor, xzibit, sizeof(xzibit))<0)
+  {
+    perror("write");
+    printf("Write of xzibit failed. Exiting.\n");
+    return -1;
+  }
+  if (close(temp_descriptor)<0)
+  {
+    perror("close");
+    printf("close has failed. Exiting.\n");
+    return -1;
   }
 
 
