bf8587e8ac6d1c866916594cc5b7c6eb80c99ba6
Jon Moroney <jmoroney@hawaii.edu>
2016-05-31 12:45:32 -1000

    Added write permissions in user directory creation

--- source/cmtp_adduser.c
+++ source/cmtp_adduser.c
@@ -70,6 +70,11 @@ int main(int argc, char * argv[])
     perror("create_verify_dir");
     return -1;
   }
+  if (chmod(user_path, S_IWOTH|S_IROTH|S_IWGRP|S_IRGRP)<0)
+  {
+    perror("chmod");
+    return -1;
+  }
   unsigned char user_publickey[crypto_sign_ed25519_PUBLICKEYBYTES];
   unsigned char user_secretkey[crypto_sign_ed25519_SECRETKEYBYTES];
   crypto_sign_ed25519_keypair(user_publickey, user_secretkey);
