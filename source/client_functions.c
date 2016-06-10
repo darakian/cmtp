37286ae4ab75be9d55bb47f66d03b10b759ce45f
Jon Moroney <jmoroney@hawaii.edu>
2016-06-10 12:23:36 -1000

    More debug

--- source/client_functions.c
+++ source/client_functions.c
@@ -868,10 +868,11 @@ int32_t display_message(char * message_path, char * private_key_buffer, char * p
 		print_to_log("crypto_box_seal_open failed to decrypt message", LOG_ERR);
 		return -1;
 	}
-	for (uint64_t i = 0; i<(message_length-crypto_box_SEALBYTES); i++)
-	{
-		printf("%x", &(plain_message_body+i));
-	}
+	print_buffer(plain_message_body, (message_length-crypto_box_SEALBYTES), "plaintext", (message_length-crypto_box_SEALBYTES), 1);
+	// for (uint64_t i = 0; i<(message_length-crypto_box_SEALBYTES); i++)
+	// {
+	// 	printf("%x", plain_message_body[i]);
+	// }
 	printf("\n");
 	return 0;
 }
